96ae9ac5214055b96a2a3dbba89da8a456c2269c
Sisi Vieira <cicy886@gmail.com>
2021-06-19 17:11:42 -0700

    get value from first question

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("#suggester").submit(function(event) {
     event.preventDefault();
     const personName = $("input#name").val();
+    const codingSpeed = $("input:radio[name=codingSpeed]:checked").val();
 
   });
 });
