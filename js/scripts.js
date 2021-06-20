398acaa42f972d5880740a82d9214a0b34d2a6cf
Sisi Vieira <cicy886@gmail.com>
2021-06-19 17:47:48 -0700

    add user's name to the result

--- js/scripts.js
+++ js/scripts.js
@@ -9,22 +9,31 @@ $(document).ready(function() {
     const feature =$("#feature").val();
 
     if (codingSpeed === "complex" && performance === "largeApplication" && program === "visualStudio") {
+      $(".name").text(personName);
       $("#cSharp").show();
     }else if (codingSpeed === "complex" && performance === "interpretive" && program === "visualStudio") {
+      $(".name").text(personName);
       $("#cSharpRuby").show();
     }else if (codingSpeed === "complex" && performance === "largeApplication" && program === "apatanaStudio") {
+      $(".name").text(personName);
       $("#python").show();
     }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "visualStudio") {
+      $(".name").text(personName);
       $("#python").show();
     } else if (codingSpeed === "fast" && performance === "interpretive" && program === "apatanaStudio") {
+      $(".name").text(personName);
       $("#codingProgram").show();
     }else if (codingSpeed === "fast" && performance === "interpretive" && program === "visualStudio") {
+      $(".name").text(personName);
       $("#pythonRuby").show();
     }else if (codingSpeed === "complex" && performance === "interpretive" && program === "apatanaStudio") {
+      $(".name").text(personName);
       $("#ruby").show();
     }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "apatanaStudio") {
+      $(".name").text(personName);
       $("#python").show();
     }else {
+      $(".name").text(personName);
       $("#pythonRuby").show();
     }
   });
