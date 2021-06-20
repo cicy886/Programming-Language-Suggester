2105ef3f3b38f13f2442a7dc497aece712d27998
Sisi Vieira <cicy886@gmail.com>
2021-06-19 18:03:10 -0700

    show different result without reflashing the page: remove id

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,66 @@ $(document).ready(function() {
     const feature =$("#feature").val();
 
     if (codingSpeed === "complex" && performance === "largeApplication" && program === "visualStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("cSharp");
+      });
       $(".name").text(personName);
       $("#cSharp").show();
     }else if (codingSpeed === "complex" && performance === "interpretive" && program === "visualStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("cSharpRuby");
+      });
       $(".name").text(personName);
       $("#cSharpRuby").show();
     }else if (codingSpeed === "complex" && performance === "largeApplication" && program === "apatanaStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("python");
+      });
       $(".name").text(personName);
       $("#python").show();
     }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "visualStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("python");
+      });
       $(".name").text(personName);
       $("#python").show();
     } else if (codingSpeed === "fast" && performance === "interpretive" && program === "apatanaStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("codingProgram");
+      });
       $(".name").text(personName);
       $("#codingProgram").show();
     }else if (codingSpeed === "fast" && performance === "interpretive" && program === "visualStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("pythonRuby");
+      });
       $(".name").text(personName);
       $("#pythonRuby").show();
     }else if (codingSpeed === "complex" && performance === "interpretive" && program === "apatanaStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("ruby");
+      });
       $(".name").text(personName);
       $("#ruby").show();
     }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "apatanaStudio") {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("python");
+      });
       $(".name").text(personName);
       $("#python").show();
     }else {
+      $("button").click(function() {
+        $("p").removeId();
+        $("p").addId("pythonRuby");
+      });
       $(".name").text(personName);
       $("#pythonRuby").show();
     }
