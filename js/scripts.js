4a3103dc8a56eef80310796d56a2cc8a7b6d31b6
Sisi Vieira <cicy886@gmail.com>
2021-06-19 17:41:33 -0700

    use branching to return the result

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,25 @@ $(document).ready(function() {
     const program = $("input:radio[name=program]:checked").val();
     const whatToCreate =$("#whatToCreate").val();
     const feature =$("#feature").val();
+
+    if (codingSpeed === "complex" && performance === "largeApplication" && program === "visualStudio") {
+      $("#cSharp").show();
+    }else if (codingSpeed === "complex" && performance === "interpretive" && program === "visualStudio") {
+      $("#cSharpRuby").show();
+    }else if (codingSpeed === "complex" && performance === "largeApplication" && program === "apatanaStudio") {
+      $("#python").show();
+    }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "visualStudio") {
+      $("#python").show();
+    } else if (codingSpeed === "fast" && performance === "interpretive" && program === "apatanaStudio") {
+      $("#codingProgram").show();
+    }else if (codingSpeed === "fast" && performance === "interpretive" && program === "visualStudio") {
+      $("#pythonRuby").show();
+    }else if (codingSpeed === "complex" && performance === "interpretive" && program === "apatanaStudio") {
+      $("#ruby").show();
+    }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "apatanaStudio") {
+      $("#python").show();
+    }else {
+      $("#pythonRuby").show();
+    }
   });
 });
