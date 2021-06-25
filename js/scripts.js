722d34351b44126269d503b2c16e62a5d6e1cd8c
Sisi Vieira <cicy886@gmail.com>
2021-06-24 17:07:51 -0700

    make the if/else statement nested inside another if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -32,30 +32,43 @@ $(document).ready(function() {
     const whatToCreate =$("#whatToCreate").val();
     const feature =$("#feature").val();
 
-    if (codingSpeed === "complex" && performance === "largeApplication" && program === "visualStudio") {
-      $(".name").text(personName);
-      replace(1);
-    }else if (codingSpeed === "complex" && performance === "interpretive" && program === "visualStudio") {
-      $(".name").text(personName);
-      replace(2);
-    }else if (codingSpeed === "complex" && performance === "largeApplication" && program === "aptanaStudio") {
-      $(".name").text(personName);
-      replace(3)
-    }else if (codingSpeed === "fast" && performance === "largeApplication" && program === "visualStudio") {
-      $(".name").text(personName);
-      replace(3);
-    } else if (codingSpeed === "fast" && performance === "interpretive" && program === "aptanaStudio") {
-      $(".name").text(personName);
-      replace(4);
-    }else if (codingSpeed === "fast" && performance === "interpretive" && program === "visualStudio") {
-      $(".name").text(personName);
-      replace(5);
-    }else if (codingSpeed === "complex" && performance === "interpretive" && program === "aptanaStudio") {
-      $(".name").text(personName);
-      replace(6);
-    }else {
-      $(".name").text(personName);
-      replace(5);
+    if (codingSpeed === "complex") {
+      if (performance === "largeApplication") {
+        if (program === "visualStudio") {
+          $(".name").text(personName);
+          replace(1);
+        } else {
+          $(".name").text(personName);
+          replace(3);
+        }
+      } else {
+        if (program === "visualStudio") {
+          $(".name").text(personName);
+          replace(2);
+        } else {
+          $(".name").text(personName);
+          replace(6);
+        }
+      }
+    } else {
+      if (performance === "largeApplication") {
+        if (program === "visualStudio") {
+          $(".name").text(personName);
+          replace(3);
+        } else{
+          $(".name").text(personName);
+          replace(5);
+        }
+      } else {
+        if (program === "visualStudio") {
+          $(".name").text(personName);
+          replace(5);
+        } else {
+          $(".name").text(personName);
+          replace(4);
+        }
+      }
     }
+
   });
 });
